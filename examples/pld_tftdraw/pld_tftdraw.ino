234b6121d8a3bca185335e809c0253d0810fd765
ddrd <ddrd001@gmail.com>
2017-11-10 15:12:02 +0300

    Adjusted example code.

--- examples/pld_tftdraw/pld_tftdraw.ino
+++ examples/pld_tftdraw/pld_tftdraw.ino
@@ -7,19 +7,20 @@
 Adafruit_ILI9341 tft = Adafruit_ILI9341(PLDuino::LCD_CS, PLDuino::LCD_DC);
 PLDTouch touch(PLDuino::TOUCH_CS, PLDuino::TOUCH_IRQ);
 
+
 void setup()
 {
   // Set pin modes and initialize stuff
   // NB: This line is necessary in all sketches which use PLDuino library stuff.
   PLDuino::init();
   
-  // Power-on LCD and set it up
+  // Power-on the LCD and set it up
   PLDuino::enableLCD();
   tft.begin();
   tft.setRotation(3);
 
-  // Initialize touch screen
-  touch.init(1);
+  // Initialize the touch screen
+  touch.init(PLDTOUCH_ROTATE_180);
 
   // Clear the screen.
   tft.fillScreen(ILI9341_BLACK);
@@ -34,22 +35,22 @@ void loop()
     // Read touch position
     Point pt = touch.read();
 
-    // Draw line between current touch and previous touch, if any
-    if (prev_pt.x != -1)
+    // Draw a line between current touch and previous touch, if any
+    if (prev_pt.isValid() && pt.isValid())
       tft.drawLine(
         prev_pt.x, prev_pt.y,
         pt.x, pt.y,
-        0xffff
+        ILI9341_WHITE
       );
     
     // Update previous touch position. Now it is current touch.
-    prev_pt = pt;      
+    prev_pt = pt;
   }
   else
   {
     // Screen isn't touched currently. 
     // Clear previous touch position.
-    prev_pt.x = -1;
+    if (prev_pt.isValid())
+      prev_pt = Point();
   }
 }
-
